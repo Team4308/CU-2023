18869c1013574b6c24eee5a5b00d607161ce0ddc
Mark Peng <mrwhy132markmin@gmail.com>
2023-02-22 16:28:38 -0500

    new library update w/ template drivesystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,12 @@ import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
 
 public final class Constants {
     public static class Mapping {
+        public static class Drive {
+            public static int frontLeft = 0;
+            public static int backLeft = 2;
+            public static int frontRight = 1;
+            public static int backRight = 3;
+        }
     }
 
     public static class Generic {
@@ -15,11 +21,79 @@ public final class Constants {
             public static double kInputDeadband = 0.14;
         }
 
-            public static class Stick {
-                public static double kInputScale = 2;
+        public static class Stick {
+            public static double kInputScale = 2;
+        }
+
+        public static class Drive {
+            public static class Kinematics {
+                public static final double kWheelDiameter = 6; // In Inches
+                public static final double kInchesPerRotation = kWheelDiameter * Math.PI;
+                public static final double kSensorUnitsPerRotation = 2048; // 2048 for talonfx
+                public static final double kEncoderInchesPerCount = kInchesPerRotation / kSensorUnitsPerRotation;
+
+                public static final double kGearRatio = (14.0 * 16.0) / (50.0 * 48.0);
             }
+
+            public static class Power {
+                public static double kOpenLoopRamp = 0.0;
+                public static double kClosedLoopRamp = 0.0;
+
+                public static StatorCurrentLimitConfiguration kStatorCurrentLimit = new StatorCurrentLimitConfiguration(
+                        false, 35, 40, 100);
+            }
+
+            public static class AngleControl {
+                public static double kP = 0.005;
+                public static double kI = 0.0;
+                public static double kD = 0.5;
+                public static double kTolerance = 0.5;
+            }
+
+            public static class VelocityControl {
+                public static int profileSlot = 0;
+
+                public static class Left {
+                    public static double kP = 0.055;
+                    public static double kI = 0.0;
+                    public static double kD = 1.45;
+                    public static double kF = 0.0468;
+                }
+
+                public static class Right {
+                    public static double kP = 0.055;
+                    public static double kI = 0.0;
+                    public static double kD = 1.45;
+                    public static double kF = 0.0468;
+                }
+            }
+
+            public static class MotionMagic {
+                public static int profileSlot = 1;
+
+                public static int maxVel = 15000;
+                public static int maxAcc = 6000;
+
+                public static class Left {
+                    public static double kP = 0.3;
+                    public static double kI = 0.0;
+                    public static double kD = 0.01;
+                    public static double kF = 0.0;
+                }
+
+                public static class Right {
+                    public static double kP = 0.3;
+                    public static double kI = 0.0;
+                    public static double kD = 0.01;
+                    public static double kF = 0.0;
+                }
+            }
+        }
     }
 
     public static class DynConfig {
+        public static class Drive {
+            public static double VelocityDriveRPM = 6300;
+        }
     }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,51 +4,71 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import java.util.ArrayList;
+
+import ca.team4308.absolutelib.control.JoystickHelper;
+import ca.team4308.absolutelib.control.XBoxWrapper;
+import ca.team4308.absolutelib.math.Vector2;
+import ca.team4308.absolutelib.math.DoubleUtils;
+import ca.team4308.absolutelib.wrapper.LogSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.DriveCommand;
+import frc.robot.subsystems.DriveSystem;
+import edu.wpi.first.math.Vector;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  public final ArrayList<LogSubsystem> subsystems = new ArrayList<LogSubsystem>();
+  private final DriveSystem m_driveSystem;
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  private final DriveCommand driveCommand;
+
+  public final XBoxWrapper stick = new XBoxWrapper(0);
+  public final XBoxWrapper stick2 = new XBoxWrapper(1);
+
   public RobotContainer() {
-    // Configure the trigger bindings
+
+    m_driveSystem = new DriveSystem();
+    subsystems.add(m_driveSystem);
+
+    driveCommand = new DriveCommand(m_driveSystem, () -> getDriveControl());
+    m_driveSystem.setDefaultCommand(driveCommand);
+
     configureBindings();
   }
 
   /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
    * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
   }
 
   /**
@@ -56,8 +76,17 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+
+  public Vector2 getDriveControl() {
+    double throttle = DoubleUtils.normalize(stick.getLeftY());
+    double turn = DoubleUtils.normalize(-stick.getRightX());
+
+    Vector2 control = new Vector2(turn, throttle);
+    control = JoystickHelper.ScaledAxialDeadzone(control, Constants.Config.Input.kInputDeadband);
+    control = JoystickHelper.scaleStick(control, Constants.Config.Stick.kInputScale);
+    control = JoystickHelper.clampStick(control);
+
+    return control;
   }
+  
 }
