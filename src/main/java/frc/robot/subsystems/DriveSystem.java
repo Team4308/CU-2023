fed693c42a8b36d09e5d1532f51867f764eb6adb
Ar-Vi <aryavishnu06@gmail.com>
2023-02-28 17:24:56 -0500

    Updated DriveSystem with CIMS/TalonSRX

--- src/main/java/frc/robot/subsystems/DriveSystem.java
+++ src/main/java/frc/robot/subsystems/DriveSystem.java
@@ -8,6 +8,7 @@ import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import ca.team4308.absolutelib.wrapper.drive.TankDriveSubsystem;
 import edu.wpi.first.util.sendable.Sendable;
@@ -17,25 +18,42 @@ public class DriveSystem extends TankDriveSubsystem {
     // Master Controllers
     public final TalonFX masterLeft, masterRight;
     // Slave Controllers
-    private final TalonFX slaveLeft, slaveRight;
+    private final TalonSRX slaveLeft, slaveRight;
 
     // Controllers
-    private ArrayList<TalonFX> controllers = new ArrayList<TalonFX>();
+    private ArrayList<TalonFX> controllersFX = new ArrayList<TalonFX>();
+    private ArrayList<TalonSRX> controllersSRX = new ArrayList<TalonSRX>();
 
     // Init
     public DriveSystem() {
         // Setup and Add Controllers
         masterLeft = new TalonFX(Constants.Mapping.Drive.frontLeft);
-        controllers.add(masterLeft);
+        controllersFX.add(masterLeft);
         masterRight = new TalonFX(Constants.Mapping.Drive.frontRight);
-        controllers.add(masterRight);
-        slaveLeft = new TalonFX(Constants.Mapping.Drive.backLeft);
-        controllers.add(slaveLeft);
-        slaveRight = new TalonFX(Constants.Mapping.Drive.backRight);
-        controllers.add(slaveRight);
+        controllersFX.add(masterRight);
+        slaveLeft = new TalonSRX(Constants.Mapping.Drive.backLeft);
+        controllersSRX.add(slaveLeft);
+        slaveRight = new TalonSRX(Constants.Mapping.Drive.backRight);
+        controllersSRX.add(slaveRight);
 
+        for (TalonSRX talon : controllersSRX) {
+                talon.configFactoryDefault(Constants.Generic.timeoutMs);
+            }
+    
+            // Change Config For All Controllers
+            for (TalonSRX talon : controllersSRX) {
+                talon.configFactoryDefault(Constants.Generic.timeoutMs);
+                talon.configOpenloopRamp(Constants.Config.Drive.Power.kOpenLoopRamp, Constants.Generic.timeoutMs);
+                talon.configClosedloopRamp(Constants.Config.Drive.Power.kClosedLoopRamp, Constants.Generic.timeoutMs);
+                talon.setNeutralMode(NeutralMode.Brake);
+                talon.configNeutralDeadband(0.001, Constants.Generic.timeoutMs);
+                talon.changeMotionControlFramePeriod(5);
+                talon.configVoltageCompSaturation(12.5, Constants.Generic.timeoutMs);
+                talon.enableVoltageCompensation(true);
+            }
+    
         // Reset Config for all
-        for (TalonFX talon : controllers) {
+        for (TalonFX talon : controllersFX) {
             talon.configFactoryDefault(Constants.Generic.timeoutMs);
         }
 
@@ -45,12 +63,12 @@ public class DriveSystem extends TankDriveSubsystem {
 
         // Set slaves to follow masters
         slaveLeft.follow(masterLeft);
-        slaveLeft.setInverted(TalonFXInvertType.FollowMaster);
+       
         slaveRight.follow(masterRight);
-        slaveRight.setInverted(TalonFXInvertType.FollowMaster);
+       
 
         // Change Config For All Controllers
-        for (TalonFX talon : controllers) {
+        for (TalonFX talon : controllersFX) {
             talon.configFactoryDefault(Constants.Generic.timeoutMs);
 
             talon.configOpenloopRamp(Constants.Config.Drive.Power.kOpenLoopRamp, Constants.Generic.timeoutMs);
