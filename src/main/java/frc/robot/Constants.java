1990a34f1fb8c24d6bff5af9b9251ff378959f2d
Leon <lzhang219@gmail.com>
2023-03-25 08:53:04 -0400

    Halved autoanglecontrol proportional gain

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -42,7 +42,7 @@ public final class Constants {
                 public static double kD = 0.0;
             }
             public static class AutoAngleControl {
-                public static double kP = 0.002;
+                public static double kP = 0.001;
                 public static double kI = 0.0001;
                 public static double kD = 0.0;
             }
