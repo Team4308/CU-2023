bbfb9efad229fb0a2e15307375a4f27c776bff13
Leon <lzhang219@gmail.com>
2023-04-03 10:19:34 -0400

    Turned paralleldeadlinegroup into parallelracegroup Increased time limit from 1.75 to 2 as they happen simultaneously now

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -84,6 +84,13 @@ public final class Constants {
                 public static double kTolerance = 0.5;
             }
 
+            public static class GyroTurnControl {
+                public static double kP = 0.005;
+                public static double kI = 0.0;
+                public static double kD = 0.0;
+                public static double kTolerance = 3.0;
+            }
+
             public static class RangeControl {
                 public static double kP = 0.01;
                 public static double kI = 0.0;
